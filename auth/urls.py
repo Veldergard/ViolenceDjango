39f7f75d3fa194448088a13c038bea6d63c49fac
veldergard <veldergard@gmail.com>
2025-05-05 02:15:55 +0300

    auth

---
+++ auth/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static  # Static files serving
+from django.urls import path
+
+from auth import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('login/', views.login_page, name='login_page'),  # Login page
+    path('register/', views.register_page, name='register_page'),  # Registration page
+]
+
+# Serve media files if DEBUG is True (development mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
