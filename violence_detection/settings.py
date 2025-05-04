6ed7b33a2c578a7828f75bcc94b189cf27b4a6c0
veldergard <veldergard@gmail.com>
2025-05-05 02:39:15 +0300

    auth

--- violence_detection/settings.py
+++ violence_detection/settings.py
@@ -115,3 +115,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# IDK
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, 'public/static')
+}
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
