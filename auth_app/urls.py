18b5681c73b1bc6a792ff7c356083b084b1573ff
veldergard <veldergard@gmail.com>
2025-05-05 04:20:58 +0300

    database

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static  # Static files serving
 from django.urls import path
 
 from auth_app import views
@@ -9,10 +7,3 @@ urlpatterns = [
     path('login/', views.login_page, name='login_page'),  # Login page
     path('register/', views.register_page, name='register_page'),  # Registration page
 ]
-
-# Serve media files if DEBUG is True (development mode)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Serve static files using staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
--- violence_detection/urls.py
+++ violence_detection/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings  # Application settings
+from django.contrib import admin  # Django admin module
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static  # Static files serving
+from django.urls import include
+from django.urls import path  # URL routing
 
 urlpatterns = [
     path('auth/', include('auth_app.urls')),
     path('polls/', include('polls_app.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Serve media files if DEBUG is True (development mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
